67e6b20bbe8464a59fae5c4bc05d13fd0c253482
Tarsoly András <tarsolya@gmail.com>
2011-01-19 16:10:17 +0100

    Tests for Titanium WebView capabilities
    * Read document properties
    * Update node contents
    * Update field contents & values

--- Resources/app.js
+++ Resources/app.js
@@ -1,64 +1,32 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
-Titanium.UI.setBackgroundColor('#000');
 
-// create tab group
-var tabGroup = Titanium.UI.createTabGroup();
+// Whip up a basic UI ...
+Titanium.UI.setBackgroundColor('#fff');
+var nav = Titanium.UI.createTabGroup();
 
 
-//
-// create base UI tab and root window
-//
-var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
-    backgroundColor:'#fff'
-});
-var tab1 = Titanium.UI.createTab({  
-    icon:'KS_nav_views.png',
-    title:'Tab 1',
-    window:win1
+var window = Ti.UI.createWindow({ title: 'Web View Test', tabBarHidden: true });
+var mainTab = Titanium.UI.createTab({
+    icon: 'KS_nav_views.png',
+    window: window
 });
 
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 1',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
+// Craft a web view ...
+var runTests = true;
+var web = Ti.UI.createWebView({ height: '100%', width: '100%', backgroundColor: '#d0e2ff', url: 'http://iphone.zappos.com' });
+web.addEventListener('load', function(e) {
+  if(runTests) {
+    alert('Page loaded, running tests ...');
+    alert('Title: ' + web.evalJS('document.title'));
+    alert('Cookies: ' + web.evalJS("document.cookie"));
+    alert('getElementById: ' + web.evalJS('document.getElementById("view-cart").innerHTML;'));
+    alert('getElementsByTagName: ' + web.evalJS("document.getElementsByTagName('head')[0].innerHTML;"));
+    alert('Update innerHTML: ' + web.evalJS('document.getElementById("view-cart").innerHTML = "UPDATED!!!";'));
+    alert('Update form field value: ' + web.evalJS('document.getElementsByName("term")[0].value = "MAGYAR MOSTMÁ!";'));
+    runTests = false;
+  };
 });
+window.add(web);
 
-win1.add(label1);
+nav.addTab(mainTab);
+nav.open();
 
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
-});
-var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
-});
-
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win2.add(label2);
-
-
-
-//
-//  add tabs
-//
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
-
-
-// open tab group
-tabGroup.open();
